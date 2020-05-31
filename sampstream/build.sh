32f5596a7a94cc77fa258bbd7b15786af52e5c50
Chris Evans <scarybeasts@gmail.com>
2020-05-30 23:50:23 -0700

    Build tweaks to be more verbose and exit on error.

--- sampstream/build.sh
+++ sampstream/build.sh
@@ -6,7 +6,8 @@ ffmpeg -y -i input.mp3 -f u8 -ac 1 -ar 6500 input.u8
 ./u8_to_bbc
 
 
-~/Downloads/beebasm/beebasm -i ../quicdisc/quicdisc.asm
-~/Downloads/beebasm/beebasm -i sampstream.asm -do sampstream.dsd -opt 3 -v
+~/Downloads/beebasm/beebasm -i ../quicdisc/quicdisc.asm -v || exit
+~/Downloads/beebasm/beebasm -i sampstream.asm -do sampstream.dsd \
+    -opt 3 -v || exit
 
 ./merge_u4_to_dsd
