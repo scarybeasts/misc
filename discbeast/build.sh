9223c5681768f5d68903cbf30a057c38580f4ba7
Chris Evans <scarybeasts@gmail.com>
2020-07-31 15:06:37 -0700

    Add commented out command for Windows cross compile.

--- discbeast/build.sh
+++ discbeast/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 gcc -g -Wall -Werror -o hfeg2hfe hfeg2hfe.c || exit
+#x86_64-w64-mingw32-gcc -g -gdwarf-2 -Wall -Werror -o hfeg2hfe.exe hfeg2hfe.c
 ~/Downloads/beebasm/beebasm -i discutil.asm -opt 3 -v || exit
 ~/Downloads/beebasm/beebasm -i discbeast.asm -do discbeast.ssd -opt 3 -v || exit
