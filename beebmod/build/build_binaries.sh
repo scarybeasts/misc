e0b6e3b015fc8ff4a6745d2fec8a6b6ee4bd6d02
Chris Evans <scarybeasts@gmail.com>
2025-06-05 18:41:47 -0700

    Build the modmod binary as part of the binaries script.

--- beebmod/build/build_binaries.sh
+++ beebmod/build/build_binaries.sh
@@ -5,3 +5,4 @@ gcc -g ../util/modxtract.c -o bin/modxtract
 gcc -g ../util/modpatconv.c -o bin/modpatconv -lm
 gcc -g ../util/sample_adjust.c -o bin/sample_adjust -lm
 gcc -g ../util/make_channel_maps.c -o bin/make_channel_maps -lm
+gcc -g ../util/modmod.c -o bin/modmod
